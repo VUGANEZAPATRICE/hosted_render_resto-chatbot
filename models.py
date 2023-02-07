8ca3bbf6ac0eb378163557f54ee07c6fd037a4b3
unknown <pvuganeza@gmail.com>
2023-02-07 09:45:31 +0200

    changing intents to NIDA

---
+++ models.py
@@ -0,0 +1,3 @@
+# import sqlalchemy
+# driver = sqlalchemy.create_engine('postgresql://chatbot_db_krsr_user:poV4FjWjIG5KCVEYh4uld6NOpPLj72Le@dpg-cf2jsuen6mpkr6eks9j0-a/chatbot_db_krsr')
+# conn = driver.connect()
